0a30a3ee2e41c8a5ffd7647158a31f0685180b97
Jose Miguel Diez Sen <josemigueldiezsen@gmail.com>
2026-05-24 00:20:33 +0200

    Se define servicio Email

--- Negocio/Utilidades/EmailSettings.cs
+++ Negocio/Utilidades/EmailSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Negocio.Utilidades
+﻿namespace Negocio.Utilidades
 {
     /// <summary>
     /// Modelo de datos de las opciones de mail
