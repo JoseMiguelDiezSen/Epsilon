1f6cf4a64ac5d6852607d5680e0579eb642a71cf
Jose Miguel Diez <73285524+JoseMiguelDiezSen@users.noreply.github.com>
2026-09-22 19:30:36 +0200

    Se recupera visor Dicom

---
+++ Epsilon/Controllers/VisorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Epsilon.Controllers
+{
+    public class VisorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
