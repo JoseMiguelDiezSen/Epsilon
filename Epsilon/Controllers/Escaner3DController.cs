fa872de06af5ed9fc10cee8b2fa2d75cc271ac94
Jose Miguel Diez <73285524+JoseMiguelDiezSen@users.noreply.github.com>
2026-09-22 19:39:42 +0200

    Se recupera Escaner 3D

---
+++ Epsilon/Controllers/Escaner3DController.cs
@@ -0,0 +1,30 @@
+using Calipso.Security;
+using Epsilon.Renders;
+using Microsoft.AspNetCore.Mvc;
+using Negocio.Servicios;
+
+namespace Epsilon.Controllers
+{
+    public class Escaner3DController : AbstractSecurityController
+    {
+        private readonly IRazorRenderService _razorRenderService;
+
+
+
+        /// <summary>
+        /// Constructor d
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="seguridad"></param>
+        /// <param name="gestionUsuarios"></param>
+        public Escaner3DController(ILogger<Escaner3DController> logger, IRazorRenderService renderService) : base(logger)
+        {
+            _razorRenderService = renderService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
