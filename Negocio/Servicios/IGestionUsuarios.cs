a1c61021e1d01a7d8cadf5d219c98fff57a42cf0
Jose Miguel Diez <73285524+JoseMiguelDiezSen@users.noreply.github.com>
2026-09-25 18:40:27 +0200

    Se hacen actualizaciones pequeñas en Epsilon

--- Negocio/Servicios/IGestionUsuarios.cs
+++ Negocio/Servicios/IGestionUsuarios.cs
@@ -4,19 +4,42 @@ namespace Negocio.Servicios
 {
     public interface IGestionUsuarios : IServicioEpsilon
     {
-        /// <summary> Obtener un listado de usuarios. </summary>
-        //IQueryable <Usuario> GetAllUsers();
-
-        /// <summary> Obtener un usuario. </summary>
+        /// <summary> 
+        /// Obtener un usuario. 
+        /// </summary>
+        /// <param name="idUsuario"> El ID del usuario a obtener.</param>
+        /// <returns>El usuario obtenido.</returns>
         Usuario GetUser(long idUsuario);
 
-        /// <summary> Agregar un usuario. </summary>
+        /// <summary>
+        /// Agregar un nuevo usuario.
+        /// </summary>
+        /// <param name="usuario">El usuario a agregar.</param>
+        /// <returns>El usuario agregado.</returns>
         Usuario AddUser(Usuario usuario);
 
-        Usuario UpdateUser(Usuario usuario);
+        /// <summary>
+        /// Actualizar un usuario existente.
+        /// </summary>
+        /// <param name="usuario">El usuario a actualizar.</param>
+        /// <returns>El usuario actualizado.</returns>
+        public Usuario UpdateUser(Usuario usuario);
 
-        bool DeleteUser(int idUsuario);
+        /// <summary>
+        /// Eliminar un usuario existente.
+        /// </summary>
+        /// <param name="idUsuario">El ID del usuario a eliminar.</param>
+        /// <returns>True si el usuario fue eliminado, false en caso contrario.</returns>
+        public bool DeleteUser(int idUsuario);
 
+        /// <summary>
+        /// Obtener los datos de un usuario, se utiliza para mostrar la información del usuario en la vista de perfil.
+        /// </summary>
+        /// <returns>Una consulta con los datos del usuario.</returns>
         IQueryable<DatosUsuario> GetDatosUsuario();
+
+
+        /// <summary> Obtener un listado de usuarios. </summary>
+        //IQueryable <Usuario> GetAllUsers();
     }
 }
